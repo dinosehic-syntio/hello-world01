e8acf0848b78e27b8c163d6a62ef2cc016938b5b
DinoSehic <dino.sehic@syntio.net>
2021-03-03 13:30:20 +0100

    proradila iteracija kroz string

--- main/test.go
+++ main/test.go
@@ -1,21 +1,26 @@
 package main
-import "fmt"
+
+import (
+	"fmt"
+)
+
 func main() {
 	fmt.Println("Hello world!")
 	fmt.Print("Hello ")
 	fmt.Print("world!\n")
-	for i:=0; i<5; i++ {
-		fmt.Println(i+1   )
+	for i := 0; i < 5; i++ {
+		fmt.Println(i + 1)
 	}
 
 	n := "nesto"
-	i := 0
-	fmt.Println(n)
-	for i < len(n){
-		fmt.Printf("%c", n[i])
-		i++
+	fmt.Println(n[0])
+	fmt.Println(len(n))
+
+	for i, c := range n {
+		fmt.Println(i, string(c))
 	}
-	fmt.Println("Upisite prvi broj")
+
+	/*fmt.Println("Upisite prvi broj")
 	var x int
 	fmt.Scanf("%d", &x)
 	fmt.Println("Upisite drugi broj")
@@ -25,10 +30,12 @@ func main() {
 
 	fmt.Println("Funkcija zbrajanja za ", x, " i ", y, ": ", add(x,y))
 
+	fmt.Println("Novi ispis za testiranje commita!")*/
 
 }
 
-func add(a int, b int) int{
+/*func add(a int, b int) int{
 	sum := a + b
 	return sum
 }
+*/
